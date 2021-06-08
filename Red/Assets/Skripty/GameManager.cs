b1cc75f55102a72bda834b088ce28f9c6cfd1c4d
KnedlCZ <janpolak26@gmail.com>
2021-06-08 22:30:44 +0200

    Pridani Red

---
+++ Red/Assets/Skripty/GameManager.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restarDelay = 1f;
+
+    public GameObject completeUI;
+
+    public void Complete()
+    {
+        Debug.Log("vyhra");
+        completeUI.SetActive(true);
+    }
+
+    public void EndGame() 
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Znova !!");
+            Invoke("Restart",restarDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+  
+}
